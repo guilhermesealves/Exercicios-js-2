880754e3b3a0c181fcf12d08f86c5c1dace7456a
Guilherme Seleri <110575996+guilhermesealves@users.noreply.github.com>
2022-12-01 17:48:41 -0300

    EX-02

---
+++ EX-02/app.js
@@ -0,0 +1,10 @@
+function Soma(){
+    let valor = document.getElementById('valor').value
+    let valor2 = document.getElementById('horas').value
+
+    let salarioBruto =  parseFloat(valor2) * parseFloat(valor)
+
+
+   
+
+}
